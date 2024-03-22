c69491f735032ba6baf616e640669c4e66b97f1f
Rafael Morelli <rafaelmorelli01@gmail.com>
2024-03-21 23:17:33 -0300

    Começando o codigo

--- main.c
+++ main.c
@@ -1,2 +1,41 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+
+    char NomeLivro[5];
+    char NomeAutor[5];
+    char Editora[5];
+    char FaixaEtaria[5];
+
+    printf("Digite o nome do livro:");
+    fgets(NomeLivro, sizeof(NomeLivro), stdin);
+    NomeLivro[strcspn(NomeLivro, "\n")] = '\0';
+
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite o nome do autor:");
+    fgets(NomeAutor, sizeof(NomeAutor), stdin);
+    NomeAutor[strcspn(NomeAutor, "\n")] = '\0';
+
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite o nome da editora:");
+    fgets(Editora, sizeof(Editora), stdin);
+    Editora[strcspn(Editora, "\n")] = '\0';
+
+    while (getchar() != '\n')
+        ;
+
+    setlocale(LC_ALL, "Portuguese");
+    printf("Digite a faixa etária do livro:");
+    fgets(FaixaEtaria, sizeof(FaixaEtaria), stdin);
+    FaixaEtaria[strcspn(FaixaEtaria, "\n")] = '\0';
+
+    return 0;
+}
